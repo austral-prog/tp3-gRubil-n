23c4541333edfb5cc33a14ef27bd10b861884bd3
Thus spoke Zarathustra <zaratustra.business@gmail.com>
2025-08-29 11:34:43 -0300

    EJERCICIOS DE SLICE HECHOS

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,13 @@
 def slice_advanced():
     # Código a implementar utilizando input.
 
+    print("# Input")
+    iput = input("> ")
+
+    print("# Imprime en pantalla")
+    print(iput[4: : 2])
+
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+
+slice_advanced()
--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,14 @@
 def slice_simple():
     texto = "Awesome"
+
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
+    print(texto[0: 3: 1].lower())
+    print(texto[3: 6: 1].lower())
+    print(texto.lower())
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+
+slice_simple()
