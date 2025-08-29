a82d34372612c166a6d71318750a8c14788ee751
Thus spoke Zarathustra <zaratustra.business@gmail.com>
2025-08-29 12:14:09 -0300

    AL FIN COMPLETE EL IN STRING DEOOO

--- in_string.py
+++ in_string.py
@@ -1,6 +1,18 @@
 def check_vowels():
     # Código a implementar utilizando input.
 
+    print("# Input")
+    user = input("> ")
+
+    vowels = "aeiou"
+    tieneTilde = False
+
+    print("\n# Imprime en pantalla")
+    for i in vowels:
+        j = i in user.lower()
+        print(f"Contiene {i}: {j}")
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+
+check_vowels()
